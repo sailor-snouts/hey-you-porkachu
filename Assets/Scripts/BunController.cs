47b6e7b80a90c18298958e90fc4c170feb334ed8
Joshua Skinner <jkyuusai@gmail.com>
2018-08-18 16:14:50 -0700

    Add bun throwing

---
+++ Assets/Scripts/BunController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunController : MonoBehaviour {
+
+    [SerializeField, Range(0.01f, 0.1f)]
+    private float shrinkRate = 0.065f;
+	
+	void Start () {
+		
+	}	
+	
+	void Update () {
+        shrink();
+	}
+
+    private void shrink() {
+        float xScale = Mathf.SmoothStep(transform.localScale.x, 0.35f, shrinkRate);
+        float yScale = Mathf.SmoothStep(transform.localScale.y, 0.35f, shrinkRate);
+        float zScale = Mathf.SmoothStep(transform.localScale.z, 0.35f, shrinkRate);
+
+        transform.localScale = new Vector3(xScale, yScale, zScale);
+    }
+}
