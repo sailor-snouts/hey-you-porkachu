9d69076ffd5fe75e46264291dd51b7ea9e481b6f
Scott Anderson <scott@wandersoft.com>
2018-08-19 15:39:19 -0700

    #10 super rough NPC controller with waypoints

---
+++ Assets/NPCController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    public int speed = 1;
+    public GameObject[] wayPoints;
+    public int currentWayPoint = 0;
+    private int targetWayPoint = 0;
+    private int direction = 1;
+
+
+	void Start () {
+        // Start at the default first position
+        transform.position = wayPoints[currentWayPoint].transform.position;
+        UpdateTargetWayPoint();
+	}
+	
+	void Update () {
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[targetWayPoint].transform.position, step);        
+
+        var distance = Vector3.Distance(transform.position, wayPoints[targetWayPoint].transform.position);
+        if (distance <= Mathf.Epsilon)
+        {
+            UpdateTargetWayPoint();
+        }
+	}
+
+    // Walk back along the path
+    private void UpdateTargetWayPoint() 
+    {
+        currentWayPoint = targetWayPoint;
+
+        if(direction > 0) {
+            targetWayPoint++;
+            if (targetWayPoint >= wayPoints.Length)
+            {
+                targetWayPoint--;
+                direction = -1;
+            }
+        } else {
+            targetWayPoint--;
+            if (targetWayPoint < 0)
+            {
+                targetWayPoint++;
+                direction = 1;
+            }
+        }
+    }
+}
