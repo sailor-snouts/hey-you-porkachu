4d62ab146e3f2ae74072c53af0d57c0404751689
Scott Anderson <scott@wandersoft.com>
2018-08-19 09:52:27 -0700

    #3 follow camera and #11 pickup buns

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public struct PlayerAnimation
@@ -16,6 +17,9 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     public float speed;
 
+    // TODO: Trivial bun pickup count for triggering Porkachu fight
+    private int buns = 0;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,7 +27,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        UpdateAnimationState();
+        UpdateMovement();
+    }
 
+    void UpdateAnimationState()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             animator.SetInteger("WalkingDirection", PlayerAnimation.ANIMATION_WALK_LEFT);
@@ -37,7 +46,48 @@ public class PlayerController : MonoBehaviour
             animator.SetInteger("WalkingDirection", PlayerAnimation.ANIMATION_IDLE);
         }
 
+    }
+
+    void UpdateMovement()
+    {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * speed * Time.deltaTime;
+        transform.position += move * speed * Time.deltaTime;   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BunPickup")
+        {
+            PickupBun(collision.gameObject);
+            return;
+        } 
+    }
+
+    private void PickupBun(GameObject bun)
+    {
+        buns++;
+        Debug.Log("Oh shit a bun! Buns = " + buns);
+        Destroy(bun);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Porkachu")
+        {
+            MeetPorkachu();
+            return;
+        } 
+    }
+
+    private void MeetPorkachu()
+    {
+        if(buns == 3) 
+        {
+            Debug.Log("PORKACHU LOVES YOU!");
+            SceneManager.LoadSceneAsync(2);
+        } else 
+        {
+            Debug.Log("PORKACHU HUNGRY!");
+        }
     }
 }
