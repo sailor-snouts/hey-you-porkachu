a344922aee6e25bbb18fb8ca818539ff92069de0
Robert Parker <rparker@kodama.design>
2018-08-19 20:47:35 -0700

    basic dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    private Renderer rend;
+
+    private Queue<string> sentences;
+    
+    void Awake()
+    {
+        sentences = new Queue<string>();
+        rend = GetComponent<Renderer>();
+        this.setRenderers(false);
+    }
+
+    void Update()
+    {
+        if (!this.isHavingDialogue())
+        {
+            return;
+        }
+
+        if (Input.anyKeyDown)
+        {
+            this.DisplayNextSentence();
+        }
+    }
+
+    public bool isHavingDialogue()
+    {
+        return rend.enabled;
+    }
+
+    public bool hasMoreDialogue()
+    {
+        return sentences.Count > 0;
+    }
+
+    private void setRenderers(bool enabled)
+    {
+        rend.enabled = enabled;
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        this.setRenderers(true);
+        this.nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        dialogueText.text = "";
+        nameText.text = "";
+        this.setRenderers(false);
+    }
+}
