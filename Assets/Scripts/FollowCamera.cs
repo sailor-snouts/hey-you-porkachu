824dfeab2e8e00dbd70606e8bd910bc8fa4fd3df
Robert Parker <rparker@kodama.design>
2018-08-19 21:36:22 -0700

    crappy follow camera
    trying to work out the kinks

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
