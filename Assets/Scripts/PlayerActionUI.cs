504f20219107831c88a82281f0e9db9822b28814
Robert Parker <rparker@kodama.design>
2018-08-22 00:44:40 -0700

    redo action checking

--- Assets/Scripts/PlayerActionUI.cs
+++ Assets/Scripts/PlayerActionUI.cs
@@ -3,18 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerActionUI : MonoBehaviour {
+    [SerializeField]
+    private float iconCheckRadius = 1f;
+    [SerializeField]
+    private float actionCheckMagnitude = 0.8f;
+    private PlayerMovementController movement;
+    private Animator animator;
+    private SpriteRenderer sprite;
 
-    [SerializeField] GameObject playerUICanvasPrefab = null;     GameObject ui;
+    void Start()
+    {
+        this.movement = this.gameObject.GetComponentInParent<PlayerMovementController>();
+        this.animator = this.gameObject.GetComponentInParent<Animator>();
+        this.sprite = this.gameObject.GetComponent<SpriteRenderer>();
+    }
 
-    private void Start()
+    void Update()
     {
-        ui = Instantiate(playerUICanvasPrefab, transform.position, Quaternion.identity, transform);
-        Hide();
-    } 
-    public void Show()     {         ui.SetActive(true);
-    } 
-    public void Hide() 
+        this.DisplayTipIcon();
+        if(Input.GetButtonDown("Fire1"))
+        {
+            this.TriggerAction();
+        }
+    }
+
+    void TriggerAction()
     {
-        ui.SetActive(false);
-    } 
+        Vector2 checkDirection;
+        switch(this.movement.GetDirection())
+        {
+            case PlayerAnimation.ANIMATION_WALK_UP:
+                checkDirection = new Vector2(0, 1);
+                break;
+            case PlayerAnimation.ANIMATION_WALK_RIGHT:
+                checkDirection = new Vector2(1, 0);
+                break;
+            case PlayerAnimation.ANIMATION_WALK_DOWN:
+                checkDirection = new Vector2(0, -1);
+                break;
+            case PlayerAnimation.ANIMATION_WALK_LEFT:
+                checkDirection = new Vector2(-1, 0);
+                break;
+            default:
+                return;
+        }
+
+        Transform parent = this.transform.parent;
+        Debug.DrawLine(parent.position, parent.position + (Vector3) checkDirection.normalized * this.actionCheckMagnitude, Color.red);
+        RaycastHit2D[] hitColliders = Physics2D.RaycastAll(parent.position, checkDirection.normalized, this.actionCheckMagnitude);
+        foreach (RaycastHit2D col in hitColliders)
+        {
+            GameObject obj = col.transform.gameObject;
+            switch (obj.tag)
+            {
+                case "BunPickup":
+                    break;
+                case "Porkachu":
+                    break;
+                case "NPC":
+                    Debug.Log("starting dialogue");
+                    DialogueTrigger dialogue = obj.GetComponent<DialogueTrigger>();
+                    dialogue.TriggerDialogue();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void DisplayTipIcon()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(this.transform.position, this.iconCheckRadius);
+        bool isShowingIcon = false;
+        foreach (Collider2D col in hitColliders)
+        {
+            if(col.gameObject.GetComponent<Actionable>())
+            {
+                isShowingIcon = true;
+                break;
+            }
+        }
+        this.sprite.enabled = isShowingIcon;
+    }
 }
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public struct PlayerAnimation
+{
+    public const int ANIMATION_IDLE = 0;
+    public const int ANIMATION_WALK_UP = 1;
+    public const int ANIMATION_WALK_RIGHT = 2;
+    public const int ANIMATION_WALK_DOWN = 3;
+    public const int ANIMATION_WALK_LEFT = 4;
+}
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    public string minigameScene;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private Animator animator;
+    private Rigidbody2D r2bd;
+    private Vector2 move;
+    private int facing;
+
+    public int GetDirection()
+    {
+        return this.facing;
+    }
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        r2bd = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        this.HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float veritcal = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontal) > Mathf.Abs(veritcal))
+        {
+            // left or right
+            int dir = horizontal > 0 ? 1 : -1;
+            this.facing = horizontal > 0 ? PlayerAnimation.ANIMATION_WALK_RIGHT : PlayerAnimation.ANIMATION_WALK_LEFT;
+            this.animator.SetInteger("WalkingDirection", this.facing);
+            this.move = new Vector2(1 * dir, 0);
+            if ((dir == 1 && this.transform.localScale.x > 0) || (dir == -1 && this.transform.localScale.x < 0))
+            {
+                Vector3 theScale = this.transform.localScale;
+                theScale.x *= -1;
+                this.transform.localScale = theScale;
+            }
+        }
+        else if (Mathf.Abs(horizontal) < Mathf.Abs(veritcal))
+        {
+            // top or bottom
+            int dir = veritcal > 0 ? 1 : -1;
+            this.facing = veritcal > 0 ? PlayerAnimation.ANIMATION_WALK_UP : PlayerAnimation.ANIMATION_WALK_DOWN;
+            this.animator.SetInteger("WalkingDirection", this.facing);
+            this.move = new Vector2(0, 1 * dir);
+        }
+        else
+        {
+            this.animator.SetInteger("WalkingDirection", PlayerAnimation.ANIMATION_IDLE);
+            this.move = Vector2.zero;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.r2bd.velocity = this.move * this.speed;
+    }
+}
