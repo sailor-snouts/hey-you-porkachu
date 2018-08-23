bd80e0eacb5c00eb054d57c96a33ae33454a3abd
Scott Anderson <scott@wandersoft.com>
2018-08-22 21:51:54 -0700

    Oh my asset cleanup and new tiles and Doors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private SpriteRenderer open;
+    private SpriteRenderer closed;
+
+    private bool isOpen;
+    private bool isLocked;
+
+	void Start () {
+        foreach ( SpriteRenderer doorSprite in GetComponentsInChildren<SpriteRenderer>() ) {
+            if(doorSprite.name == "open") {
+                open = doorSprite;
+            } else if(doorSprite.name == "closed") {
+                closed = doorSprite;
+            }
+        }
+
+        isOpen = false;
+	}
+
+    void Update () {
+        open.enabled = isOpen;
+        closed.enabled = !isOpen;
+	}
+
+    void Open() {
+        isOpen = true;
+    }
+
+    void Close() {
+        isOpen = false;
+    }
+
+}
