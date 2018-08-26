fdfb9a7d8041bfb47b49c3401c80f00b55eca36d
Robert Parker <rparker@kodama.design>
2018-08-26 14:32:48 -0700

    health bar

---
+++ Assets/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 10000;
+    [SerializeField]
+    private int health;
+    private SpriteMask mask;
+
+    private void Start()
+    {
+        this.mask = GetComponentInChildren<SpriteMask>();
+        this.health = this.maxHealth;
+    }
+
+    public bool IsAlive()
+    {
+        return this.health > 0;
+    }
+
+    public void Hurt(int dmg)
+    {
+        this.health -= dmg;
+        this.UpdateMask();
+    }
+
+    private void UpdateMask()
+    {
+        float percent = 1f -(float) this.health / (float) this.maxHealth;
+        this.mask.transform.localScale = new Vector3(percent, this.mask.transform.localScale.y, this.mask.transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/Battle/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 100;
+	
+    public bool isAlive()
+    {
+        return this.health > 0;
+    }
+
+    public void Hurt(int dmg)
+    {
+        this.health -= dmg;
+    }
+}
