f856567a8aedcac3c0e95288429eb8eb6713d198
Scott Anderson <scott@wandersoft.com>
2018-08-26 16:06:14 -0700

    pork logic now in place

---
+++ Assets/Scripts/Overworld/PigPickupController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigPickupController : PickupController
+{
+    public override void Pickup(Inventory inventory)
+    {
+        GameManager manager = FindObjectOfType<GameManager>();
+
+        if( manager.ChefDefeated(3)) {
+            pickupSprite.SetActive(true);
+            animating = true;
+            inventory.AddItem(pickupType);
+
+            DialogueTrigger[] dialogueTriggers = gameObject.GetComponents<DialogueTrigger>();
+            foreach (DialogueTrigger trigger in dialogueTriggers)
+            {
+                if (trigger.triggerId == 0)
+                    trigger.TriggerDialogue(FindObjectOfType<PlayerMovementController>());
+            }
+        }
+        else {
+            DialogueTrigger[] dialogueTriggers = gameObject.GetComponents<DialogueTrigger>();
+            foreach (DialogueTrigger trigger in dialogueTriggers ) {
+                if(trigger.triggerId == 1 )
+                    trigger.TriggerDialogue(FindObjectOfType<PlayerMovementController>());
+            }
+        }
+    }
+}
