e55d5cd687c6bfef040b398bb95b38568681bf63
Joshua Skinner <jkyuusai@gmail.com>
2018-08-26 01:29:43 -0700

    Create random enemy when battle scene is loaded

---
+++ Assets/Scripts/Battle/EnemyGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+    public List<GameObject> enemies;
+
+	void Start () {
+        GameObject enemy = enemies[Random.Range(0, enemies.Count)];
+        Instantiate(enemy, enemy.transform.position, Quaternion.identity);
+	}
+}
