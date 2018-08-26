6bbab67b5dd8d4768437eb2a74923413f0fd38d5
Robert Parker <rparker@kodama.design>
2018-08-26 14:51:41 -0700

    love meter

--- Assets/Scripts/Battle/HealthBar.cs
+++ Assets/Scripts/Battle/HealthBar.cs
@@ -2,18 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
     [SerializeField]
-    private int health = 100;
-	
-    public bool isAlive()
+    private bool IsHealth = true;
+    [SerializeField]
+    private int maxHealth = 100;
+    [SerializeField]
+    private SpriteRenderer healthFrame;
+    [SerializeField]
+    private SpriteRenderer loveFrame;
+    private int health;
+    private SpriteMask mask;
+
+    private void Start()
+    {
+        this.mask = GetComponentInChildren<SpriteMask>();
+        this.health = this.maxHealth;
+        this.UpdateMask();
+        if(this.IsHealth)
+        {
+            this.healthFrame.enabled = true;
+            this.loveFrame.enabled = false;
+        } else
+        {
+            this.loveFrame.enabled = true;
+            this.healthFrame.enabled = false;
+        }
+    }
+
+    public bool IsAlive()
     {
         return this.health > 0;
     }
 
-    public void Hurt(int dmg)
+    public bool IsLoved()
     {
-        this.health -= dmg;
+        return this.health >= this.maxHealth;
+    }
+
+    public void HurtLove(int dmg)
+    {
+        if(!this.IsHealth)
+        {
+            dmg *= -1;
+        }
+        this.health = Mathf.Clamp(this.health - dmg, 0, this.maxHealth);
+        this.UpdateMask();
+    }
+
+    private void UpdateMask()
+    {
+        float percent = 1f - (float) this.health / (float) this.maxHealth;
+        this.mask.transform.localScale = new Vector3(percent, this.mask.transform.localScale.y, this.mask.transform.localScale.z);
     }
 }
